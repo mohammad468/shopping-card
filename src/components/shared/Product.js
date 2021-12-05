95f396514a42b270e5732672578ade9b7f0d9876
mohammad mohseni <mohammad.mohseni468@gmail.com>
2021-12-05 14:07:33 +0330

    buttons in cards (handlers)

--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Card, Button, Col } from "react-bootstrap";
 import "./product.scss";
-import { shorten } from "../helpers/functions";
+import { shorten, isInCart, quantityCount } from "../helpers/functions";
 import { Link } from "react-router-dom";
+// *icons
+import { MdAdd, MdOutlineRemove, MdDelete } from "react-icons/md";
+
+// *context
+import { CartContext } from "../../context/CartContextProvider";
 
 const Product = ({ productData }) => {
+  const { state, dispatch } = useContext(CartContext);
+
   return (
     <Col className="my-3 d-flex justify-content-around">
       <Card className="my-cards" style={{ width: "18rem", height: "30rem" }}>
@@ -26,7 +33,46 @@ const Product = ({ productData }) => {
           </Card.Text>
         </Card.Body>
 
-        <Button className="mx-2 my-3">Add to card</Button>
+        {/* <Button className="mx-2 my-3">Add to card</Button> */}
+        {isInCart(state, productData.id) ? (
+          <div className="d-flex justify-content-center">
+            <Button
+              className="mx-2 my-3 button-added-one-to-card"
+              onClick={() =>
+                dispatch({ type: "INCREASE", payload: productData })
+              }
+            >
+              <MdAdd />
+            </Button>
+            {quantityCount(state, productData.id) === 1 && (
+              <Button
+                className="mx-2 my-3 button-added-one-to-card"
+                onClick={() =>
+                  dispatch({ type: "REMOVE_ITEM", payload: productData })
+                }
+              >
+                <MdDelete />
+              </Button>
+            )}
+            {quantityCount(state, productData.id) > 1 && (
+              <Button
+                className="mx-2 my-3 button-added-one-to-card"
+                onClick={() =>
+                  dispatch({ type: "DECREASE", payload: productData })
+                }
+              >
+                <MdOutlineRemove />
+              </Button>
+            )}
+          </div>
+        ) : (
+          <Button
+            className="mx-2 my-3 button-add-to-card"
+            onClick={() => dispatch({ type: "ADD_ITEM", payload: productData })}
+          >
+            Add to card
+          </Button>
+        )}
       </Card>
     </Col>
   );
